eaa1d041312685f7219b889ff468dcf3ff762d94
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-02 12:14:10 +0200

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Manyuchi, Carrington C on 2024/12/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
